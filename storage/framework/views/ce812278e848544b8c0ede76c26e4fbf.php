31cd36b88209553753787bd2891dc60c4188f9b5
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-14 11:59:08 +0100

    fix

--- storage/framework/views/ce812278e848544b8c0ede76c26e4fbf.php
+++ storage/framework/views/ce812278e848544b8c0ede76c26e4fbf.php
@@ -14,6 +14,7 @@
 
         <!-- Scripts -->
         
+        
         <?php echo app('Illuminate\Foundation\Vite')(['resources/scss/main.scss', 'resources/js/app.js']); ?>
     </head>
     <body>
