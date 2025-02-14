c0a96284cc5f97d0e73ceca755a6a8d5bac31b0e
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-14 11:58:18 +0100

    xx

--- storage/framework/views/ce812278e848544b8c0ede76c26e4fbf.php
+++ storage/framework/views/ce812278e848544b8c0ede76c26e4fbf.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title><?php echo e(config('app.name', 'Laravel')); ?></title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        {{-- <link rel="shortcut icon" type="image/x-icon" href="{{ Vite::asset('resources/imgs/favicon.ico') }}"> --}}
+        
 
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-        @vite(['resources/scss/main.scss', 'resources/js/app.js'])
+        
+        <?php echo app('Illuminate\Foundation\Vite')(['resources/scss/main.scss', 'resources/js/app.js']); ?>
     </head>
     <body>
 
@@ -87,29 +87,15 @@
         <div class="offcanvas__overlay-white"></div>
         <!-- Offcanvas area end-->
 
-        @include('partials.header')
+        <?php echo $__env->make('partials.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
         <main>
-            {{ $slot }}
+            <?php echo e($slot); ?>
+
         </main>
 
 
-        {{-- <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div> --}}
+        
     </body>
 </html>
+<?php /**PATH C:\laragon\www\portfolio-lujandev\resources\views/layouts/app.blade.php ENDPATH**/ ?>
