c480dac06c09ecb45ed5a8c931db1d58643ba961
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-14 12:04:43 +0100

    Update .gitignore to ignore view cache files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
 
         <!-- Scripts -->
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        
         @vite(['resources/scss/main.scss', 'resources/js/app.js'])
     </head>
     <body>
